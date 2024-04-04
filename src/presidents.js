20a0ef18c4d6e9cb973d034bfe376d6dc3fcf53c
camilaalbuq <cam.albuquer@gmail.com>
2024-04-04 18:09:54 +0200

    solved lab, including bonus, not all passed

--- src/presidents.js
+++ src/presidents.js
@@ -1,3 +1,7 @@
+//lab from week 02, day 04 - 04.04.24
+//We'll use the array methods map(), filter, reduce(), and sort()
+//to iterate over the array and format, filter, and sort the data.
+
 const presidents = [
   {
     id: 1,
@@ -412,82 +416,231 @@ const presidents = [
     tookOffice: 2021,
     leftOffice: null,
     party: "Democratic",
-  }
+  },
 ];
 
-
-
-
 // Iteration 1: Names of Presidents - `map()`
-function getNames(presidentsArr) {}
+//The map() method creates a new array populated with the results returned from the provided callback function for every element in the array.
+function getNames(presidentsArr) {
+  const presidentNames = presidentsArr.map((president) => {
+    return president.name;
+  });
+  return presidentNames;
+}
+
+//for each element inside of the array do this
+//since the map method is already applied on an array
+//then it will create also a new array "finalGrades" with the method result
+
+//testing the output
+console.log(getNames(presidents));
 
 // console.log("getNames(presidents)", getNames(presidents));
 
-
-
+//****mapping example from class today 04.04.24 */
+// //Mapping similar to "forEach" student
+// const finalGrades = students.map((theStudent) => {
+//   //lets find the project average ((project 1 + project 2)/2)
+//   //this project avg will be 40% of total grade
+//   const projectsAvg = (theStudent.firstProject + theStudent.secondProject) / 2;
+//   //final grade is finalexam 60% + projectavg 40%
+//   const finalGrade = theStudent.finalExam * 0.6 + projectsAvg * 0.4;
+//   return {
+//     //we want to return an array of objects
+//     //for each student it is an object containing name and final grade
+//     name: theStudent.name,
+//     finalGrade: Math.round(finalGrade),
+//   };
+// });
 
 // Iteration 2: Name and Party  - `map()`
-function getNamesAndParty(presidentsArr) {}
+function getNamesAndParty(presidentsArr) {
+  const presidentNamesandParty = presidentsArr.map((president) => {
+    return `${president.name} , ${president.party}`;
+  });
+  return presidentNamesandParty;
+}
 
 // console.log("getNamesAndParty(presidents)", getNamesAndParty(presidents));
 
-
-
-
 // Iteration 3: Democratic presidents - `filter()`
-function getDemocraticPresidents(presidentsArr) {}
+//the filter() method creates a new array containing only the elements that pass the test implemented by the callback function.
+function getDemocraticPresidents(presidentsArr) {
+  const filterDemocratic = presidentsArr.filter((president) => {
+    return president.party === "Democratic";
+  });
+  return filterDemocratic;
+}
 
 // console.log("getDemocraticPresidents(presidents)", getDemocraticPresidents(presidents));
 
-
-
-
 // Iteration 4: Affiliated Presidents - `filter()`
-function getAffiliatedPresidents(presidentsArr) {}
+//since no instructions, assuming that affiliated are all the presidents which party !== null
+function getAffiliatedPresidents(presidentsArr) {
+  const affiliated = presidentsArr.filter((president) => {
+    return president.party !== null;
+  });
+  return affiliated;
+}
 
-// console.log("getAffiliatedPresidents(presidents)", getAffiliatedPresidents(presidents));
+// console.log(
+//   "getAffiliatedPresidents(presidents) - these presidents are affiliated and have a party",
+//   getAffiliatedPresidents(presidents)
+// );
 
+//extra, these presidents do not have a party
+function getNoPartyPresidents(presidentsArr) {
+  const noPartyPresidenty = presidentsArr.filter((president) => {
+    return president.party === null;
+  });
+  return noPartyPresidenty;
+}
 
-
+console.log(
+  "getNoPartyPresidents(presidents) - these presidents do NOT have a party",
+  getNoPartyPresidents(presidents) // George Washington and John Tyler
+);
 
 // Iteration 5: Count Years in Office - `reduce()`
-function  countYearsInOffice(presidentsArr) {}
 
-// console.log("countYearsInOffice(presidents)", countYearsInOffice(presidents));
+//During each iteration, the reducer function receives the accumulated value
+// and returns a new value to be used as the accumulated value for the next iteration.
+//The reduce() method returns a single value, which is the final accumulated result.
 
+// count the total years that all the presidents served in office (leftOffice - tookOffice).
+// The function should then return a number representing the total years.
+// Important: You should skip the president who is still in office
+// (the president with the leftOffice property set to null) and not include them in the total years.
 
+// //first lets create new array without the latest president
+// // The pop() method removes (pops) the last element of an array.
+// // The pop() method changes the original array.
+// // The pop() method returns the removed element.
 
+const latestPresident = presidents.pop();
+presidents.pop();
 
-// Iteration 6: Count Republican Presidents - `reduce()`
-function countRepublicanPresidents(presidentsArr) {}
+// console.log("these are presidents without latest", presidents);
 
-// console.log("countRepublicanPresidents(presidents)", countRepublicanPresidents(presidents));
+let totalYearsServed = 0;
+//now we start this function with the presidents array already excluding the last
+function countYearsInOffice(presidentsArr) {
+  //find the total left office time, without including the latest president
+  const totalLeftOffice = presidentsArr.reduce((acc, curr) => {
+    return acc + curr.leftOffice;
+  }, 0);
+  //find the total took office time, without including the latest president
+  const totalTookOffice = presidentsArr.reduce((acc, curr) => {
+    return acc + curr.tookOffice;
+  }, 0);
 
+  //push the result into the totalYearsServed array
+  totalYearsServed = totalLeftOffice - totalTookOffice;
+  return Number(totalYearsServed + 4); //because the current president already served 4 years
+}
 
+console.log("countYearsInOffice(presidents)", countYearsInOffice(presidents));
+//output: countYearsInOffice(presidents) [ 232 ]
+//doesnt pass the tests because we need if statements :(
 
+//GENIOUS DOING THIS WITHOUT ONE SINGLE GOOGLE!!! O/
+//didnt pass the tests because they want an "if statement"
+
+// //example reduce from class
+// const averageCalories = menu.reduce((acc, curr) => {
+//   return acc + curr.calories; //the reduce needs to finish accumulating
+// }, 0); //so we cannot do the average here
+// //therefore we will do average after we accumulate, when we console.log
+// console.log(averageCalories / menu.length); // 278
+
+//BEFORE DOING NEXT ITERACTIONS, LET'S RETURN THE LATEST PRESIDENT TO THE ARRAY
+//The push() method adds new items to the end of an array.
+presidents.push(latestPresident);
+console.log(presidents); //this is working, included Joe Biden back
+
+// // Iteration 6: Count Republican Presidents - `reduce()`
+// function countRepublicanPresidents(presidentsArr) {
+//   //first filter the republicans into a new array
+//   const onlyRepublicans = presidentsArr.filter((president) => {
+//     return president.party === "Republican";
+//   });
+
+//   //now we can reduce to count this
+//   const republicans = onlyRepublicans.reduce((acc, curr) => {
+//     return acc + curr;
+//   });
+//   return republicans;
+// }
+// console.log(republicans);
+
+// console.log(
+//   "countRepublicanPresidents(presidents)",
+//   countRepublicanPresidents(presidents)
+// );
 
 // Iteration 7: Sort Presidents by Birth Year - `sort()`
-function sortPresidentsByBirthYear(presidentsArr) {}
+//The sort() method is used to sort the elements of an array in place.
+//This means the original array is mutated,
+//and the sorted elements are rearranged within the same array.
 
+//The function should use the sort() method to sort the presidents array by birth year,
+// from oldest to youngest.
 
+// let allBirthYears = [];
 
+// function sortPresidentsByBirthYear(presidentsArr) {
+//   //create a new array only with birthyears with map
+//   presidentsArr.forEach((president) => allBirthYears.push(president.birthYear));
+
+//   allBirthYears.sort((a, b) => a - b);
+//   return allBirthYears;
+// }
+// //doesnt pass the tests because we need to have the presidents array passed as argument :(
+// console.log(sortPresidentsByBirthYear(presidents));
+
+//Iteration 7 - sort by birth year
+function sortPresidentsByBirthYear(presidentsArr) {
+  const sortedArray = presidentsArr.sort((a, b) => {
+    return a.birthYear - b.birthYear;
+  });
+  return sortedArray;
+}
 
 // BONUS: Iteration 8 | Age At Inauguration - `map()`
+
+//The function should use the map() method to create a new array of updated president objects,
+//with a new property ageAtInauguration containing the age of each president when they took office.
 function getAgeAtInauguration(presidentsArr) {}
 
 // console.log("ageAtInauguration(presidents)", ageAtInauguration(presidents));
 
-
-
-
 // BONUS: Iteration 9 | Presidents Born After - `filter()`
-function getPresidentsBornAfter(presidentsArr, year) {}
+// getPresidentsBornAfter() that takes two arguments: the presidents array and a year.
+// The function should use the filter() method to create a new array containing
+// only the presidents who were born after the year provided as an argument.
+function getPresidentsBornAfter(presidentsArr, year) {
+  const bornAfterYear = presidentsArr.filter((president) => {
+    if (president.birthYear > year) {
+      return president.birthYear;
+    }
+  });
+  return bornAfterYear;
+}
 
 // console.log("getPresidentsBornAfter(presidents)", getPresidentsBornAfter(presidents));
 
-
-
-
 // BONUS: Iteration 10: Sort Presidents by Name - `sort()`
-function sortPresidentsByName(presidentsArr) {}
+function sortPresidentsByName(presidentsArr) {
+  const sortedArray2 = presidentsArr.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+  return sortedArray2;
+}
 
+//bonus iteration 11 count republicans
+function countRepublicanPresidents(presidentsArr) {
+  presidentsArr.filter((president) => president.party === "Republican");
+  return presidentsArr.reduce((acc) => {
+    return acc++;
+  }, 0);
+}
