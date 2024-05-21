ece53caf7bcfdd00880a9bbc3bac2cbd4307d312
Sieur Loic <loic.tramis@gmail.com>
2024-05-21 15:07:14 +0200

    Solve lab

--- src/presidents.js
+++ src/presidents.js
@@ -465,14 +465,6 @@ function countRepublicanPresidents(presidentsArr) {
 
 // Bonus: Iteration 8 | Sort Presidents by Name - `sort()`
 function sortPresidentsByName(presidentsArr) {
-    presidentsArr.sort((a, b) => {
-        if (a.name > b.name) {
-            return 1;
-        } else if (a.name < b.name) {
-            return -1;
-        }
-        return 0;
-    });
-    console.log(presidentsArr);
+    presidentsArr.sort((a, b) => a.name.localeCompare(b.name));
     return presidentsArr;
 }
