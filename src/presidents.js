678a7b484864cf6677e5e2654c49d8148bff8bd1
Reda Dahmane <r.dahmanee@gmail.com>
2024-05-22 08:50:23 +0200

    Did not find the time to do the iteration 4 .sort

--- src/presidents.js
+++ src/presidents.js
@@ -412,54 +412,44 @@ const presidents = [
     tookOffice: 2021,
     leftOffice: null,
     party: "Democratic",
-  }
+  },
 ];
 
-
-
-
 // Iteration 1 | Names of All Presidents - `map()`
-function getNames(presidentsArr) {}
-
-
-
+function getNames(presidentsArr) {
+  return presidentsArr.map((presidents) => presidents.name);
+}
 
 // Iteration 2 | Democratic Presidents - `filter()`
-function getDemocraticPresidents(presidentsArr) {}
-
-
-
+function getDemocraticPresidents(presidentsArr) {
+  return presidentsArr.filter(
+    (presidents) => presidents.party === "Democratic"
+  );
+}
 
 // Iteration 3 | Count Years in Office - reduce()
-function  countYearsInOffice(presidentsArr) {}
-
-
-
+function countYearsInOffice(presidentsArr) {
+  return presidentsArr.reduce((accumulator, presidents) => {
+    if (presidents.leftOffice === null) {
+      return accumulator;
+    }
+    return accumulator + (presidents.leftOffice - presidents.tookOffice);
+  }, 0);
+}
+const totalYears = countYearsInOffice(presidents);
+console.log(totalYears);
 
 // Iteration 4 | Sort Presidents by Birth Year - `sort()`
 function sortPresidentsByBirthYear(presidentsArr) {}
 
-
-
-
 // Bonus: Iteration 5 | Age at Inauguration - `map()`
 function getAgeAtInauguration(presidentsArr) {}
-
-
-
 
 // Bonus: Iteration 6 | Presidents Born After - `filter()`
 function getPresidentsBornAfter(presidentsArr, year) {}
 
-
-
-
 // Bonus: Iteration 7 | Count Republican Presidents
 function countRepublicanPresidents(presidentsArr) {}
 
-
-
-
 // Bonus: Iteration 8 | Sort Presidents by Name - `sort()`
 function sortPresidentsByName(presidentsArr) {}
-
